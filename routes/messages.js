32bab3de1b154cd8f046f9c4e1df5c75d4db11e2
Marco Berardini <mberardini@evolution-man.com>
2016-09-05 14:28:36 -0700

    [client-server] Added ability to delete messages.

--- routes/messages.js
+++ routes/messages.js
@@ -69,4 +69,35 @@ router.patch('/:id', function(req, res, next){
     })
 })
 
+router.delete('/:id', function(req, res, next){
+    Message.findById(req.params.id, function(err, doc){
+        if(err){
+            return res.status(404).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+        if(!doc){
+            return res.status(404).json({
+                title: 'No Document found',
+                error: {message: 'Message could not be found'}
+            });
+        }
+        doc.content = req.body.content;
+        doc.remove(function(err, result) {
+            if(err){
+                return res.status(404).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: result
+            });
+        });
+    })
+});
+
+
 module.exports = router;
